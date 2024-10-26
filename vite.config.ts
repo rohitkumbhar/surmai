b569856ae2668de47e0c5d138f7ef248442e17df
Rohit Kumbhar <rohit@kumhbar.net>
2024-10-25 22:14:53 -0700

    Disabled PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import {VitePWA} from 'vite-plugin-pwa'
 
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
 
-  // @ts-ignore
+  /*// @ts-ignore
   const routeMatchCallback = ({request}) => {
     console.log("match callback request", request)
     return true
-  }
+  }*/
   return {
     plugins: [
       react(),
-      VitePWA({
+      /*VitePWA({
         registerType: 'autoUpdate',
         minify: false,
         devOptions: {
@@ -82,7 +81,7 @@ export default defineConfig(() => {
             }
           ]
         }
-      })
+      })*/
     ],
   }
 })
