1081fb2499793753d7cff15290d560e4a14f0d2c
Rohit Kumbhar <rohit@kumhbar.net>
2025-01-04 11:25:25 -0800

    WIP: Building Github pages

---
+++ site/src/main.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client';
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/notifications/styles.css';
+
+import { Container, Text } from '@mantine/core';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Container>
+    <Text>Hello Surmai Site</Text>
+  </Container>
+);
