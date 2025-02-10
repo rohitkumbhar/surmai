acf0763613baeabf2e1a55dcdd615b7c9e77a336
Rohit Kumbhar <rohit@kumhbar.net>
2025-02-09 17:46:15 -0800

    fix(timezone): fixes setting of timezone when creating or editing a trip

--- backend/hooks/add_timezone_to_destinations.go
+++ backend/hooks/add_timezone_to_destinations.go
@@ -17,35 +17,32 @@ func AddTimezoneToDestinations(e *core.RecordEvent, finder tzf.F) error {
 	err := json.Unmarshal([]byte((destinations)), &payload)
 
 	var updatedDestinations = make([]bt.Destination, len(payload))
-
 	if err != nil {
 		return err
 	}
 
 	for i, destination := range payload {
-
 		updatedDestination := bt.Destination{
-			Id:   destination.Id,
-			Name: destination.Name,
+			Id:          destination.Id,
+			Name:        destination.Name,
+			CountryName: destination.CountryName,
+			StateName:   destination.StateName,
+			Latitude:    destination.Latitude,
+			Longitude:   destination.Longitude,
+			TimeZone:    destination.TimeZone,
 		}
 
-		updatedDestinations[i] = updatedDestination
-
 		if destination.Latitude != "" && destination.Longitude != "" {
-
 			timezone := destination.TimeZone
 			if timezone == "" {
 				lat, _ := strconv.ParseFloat(destination.Latitude, 64)
 				long, _ := strconv.ParseFloat(destination.Longitude, 64)
-				timezone = finder.GetTimezoneName(long, lat)
+				updatedDestination.TimeZone = finder.GetTimezoneName(long, lat)
 			}
-
-			updatedDestination.CountryName = destination.CountryName
-			updatedDestination.StateName = destination.StateName
-			updatedDestination.Latitude = destination.Latitude
-			updatedDestination.Longitude = destination.Longitude
-			updatedDestination.TimeZone = timezone
 		}
+
+		updatedDestinations[i] = updatedDestination
+
 	}
 
 	record.Set("destinations", updatedDestinations)
