decc8dcf9d2456b659d59bf28990643a2d3f8be9
Rohit Kumbhar <rohit@kumbhar.net>
2025-05-18 18:18:54 -0700

    feat: headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    headless: false,
+    headless: true,
     baseURL: 'http://localhost:6173',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
