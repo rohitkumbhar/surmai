eedb40f7280430f9c2eac06de45f1e1aa84e32a2
Rohit Kumbhar <rohit@kumbhar.net>
2025-08-11 21:24:34 -0700

    fix: updated litestream configuration to be explicit

--- backend/litestream-init.sh
+++ backend/litestream-init.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
-# Check if all required S3 environment variables are available and not empty
-if [ -n "${SURMAI_DB_BKP_BUCKET_URL}" ] && [ -n "${SURMAI_DB_BKP_ACCESS_KEY_ID}" ] && [ -n "${SURMAI_DB_BKP_ACCESS_KEY_SECRET}" ]; then
+# Check if database backup is enabled
+if [ "${SURMAI_DB_BKP_ENABLED}" = "true" ]; then
 
-  # If all variables are available, use litestream
+  # If enabled, use litestream
   pb_data_dir=${PB_DATA_DIRECTORY:-/pb_data}
 
-  if [ -f ${pb_data_dir} ]; then
-  	echo "Database already exists, skipping restore"
+  if [ -d "${pb_data_dir}/data.db" ]; then
+    echo "Database already exists, skipping restore"
   else
-  	echo "No database found, restoring from replica if exists"
-  	litestream restore -if-db-not-exists -if-replica-exists -o "${pb_data_dir}/data.db" --replica s3 "${pb_data_dir}/data.db"
+    echo "No database found, restoring from replica if exists"
+    litestream restore -if-db-not-exists -if-replica-exists -o "${pb_data_dir}/data.db" --replica s3 "${pb_data_dir}/data.db"
   fi
 
   echo "Starting Surmai server with litestream replicate"
