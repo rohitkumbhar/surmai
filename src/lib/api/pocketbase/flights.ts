5001570e03f3996e2e10ffda526777bbfad78edd
Rohit Kumbhar <rohit@kumbhar.net>
2026-02-06 18:46:08 -0800

    bug(273): updating query limits to 150

--- src/lib/api/pocketbase/flights.ts
+++ src/lib/api/pocketbase/flights.ts
@@ -23,33 +23,4 @@ export const getFlightRoute = (flightNumber: string): Promise<FlightRoute> => {
     method: 'GET',
     signal: AbortSignal.timeout(5 * 60 * 1000),
   });
-
-  // return fetch(` https://api.adsbdb.com/v0/callsign/${sanitizedFlightNumber}`)
-  //   .then(r => r.json())
-  //   .then(data => {
-  //     console.log(data);
-  //     const route = data.response.flightroute;
-  //
-  //     if (!route) {
-  //       return Promise.reject('Invalid Flight Number');
-  //     }
-  //
-  //     return {
-  //       callsign: route.callsign,
-  //       callsign_iata: route.callsign_iata,
-  //       callsign_icao: route.callsign_icao,
-  //       airline: {
-  //         name: route.airline?.name,
-  //         code: route.airline?.iata,
-  //       },
-  //       origin: {
-  //         name: route.origin?.name,
-  //         iataCode: route.origin?.iata_code,
-  //       },
-  //       destination: {
-  //         name: route.destination?.name,
-  //         iataCode: route.destination?.iata_code,
-  //       },
-  //     };
-  //   });
 };
