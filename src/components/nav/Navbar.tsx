b24ee04f64734929adf7818aee2ec69143891463
emjay4023 <mjanaec@gmail.com>
2026-08-09 11:53:18 -0500

    modify logo size properties

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Anchor, Center, Group, rem, Stack, Text, Tooltip, UnstyledButton } from '@mantine/core';
+import { Image, Anchor, Center, Group, rem, Stack, Text, Tooltip, UnstyledButton } from '@mantine/core';
 import { useClickOutside } from '@mantine/hooks';
 import {
   IconChartBar,
@@ -15,7 +15,6 @@ import { useLocation, useNavigate } from 'react-router-dom';
 
 import classes from './Navbar.module.css';
 import { isAdmin, logoutCurrentUser } from '../../lib/api';
-import { NavLogo } from '../logo/NavLogo.tsx';
 
 interface NavbarLinkProps {
   icon: typeof IconHome2;
@@ -77,7 +76,12 @@ export function Navbar({ close }: NavbarProps) {
       </UnstyledButton>
       <Center>
         <Anchor href={'/'} target={'_self'}>
-          <NavLogo size={30} />
+          <Image
+            h="auto"
+            w="100%"
+            fit="contain"
+            src="/icons/logo.svg"
+          />
         </Anchor>
       </Center>
       <div className={classes.navbarMain}>
